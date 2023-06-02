14bd9c8519e488bd526d40e358176ccacfacb610
Maxime Aube <maxime.aube@protonmail.com>
2023-06-02 18:05:52 +0200

    refacto structure de fichiers avec front et back séparés

--- src/database/database.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { SubscriptionSchema } from "./schema/SubscriptionSchema";
-
-// ajout du modèle mongoose pour les souscriptions
-// pour plusieurs modèles, un système d'imports dynamiques serait indiqué
-mongoose.model(SubscriptionSchema.schemaName, SubscriptionSchema.create());
-
-export default mongoose;
--- src/types/QueryOptions.ts
+++
@@ -1,8 +0,0 @@
-export type QueryOptions = {
-  select: string;
-  limit: number;
-  sort: {
-    [fieldName: string]: "asc" | "desc" | "ascending" | "descending" | 1 | -1;
-  };
-  where: Array<object>;
-};
